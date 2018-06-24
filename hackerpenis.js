87f683d0c5a2e15513e2059cce04e273c162c78c
aawill <aawill@umich.edu>
2018-06-24 00:12:39 -0400

    ALPHA AS FUCK DUDE

--- hackerpenis.js
+++ hackerpenis.js
@@ -1,11 +1,87 @@
 var stringIndex = 0;
-var nutWidth = 5;
+var nutWidth = 9;
 var shaftHeight = 12;
+var dataString = "";
+var cursorIndex = 0;
+var nutEndReached = false;
+var lineEndReached = false;
+var linesPrinted = 0;
+var state = "BALLS"; // BALLS, SHAFT
+var inShaft = false;
+
+function reqListener () {
+  dataString = this.responseText;
+}
+
+var inputFile = new XMLHttpRequest();
+inputFile.addEventListener("load", reqListener);
+inputFile.open("GET", "HPinput.txt");
+inputFile.send();
 
 $(document).keypress(function() {
-  stringIndex++;
-  //console.log(stringIndex);
-  $('#mainWindow').append(testString[stringIndex]);
-});
+  if (state == "BALLS") {
+    for (var i = 0; i < nutWidth / 3; ++i) {
+      // space between nuts
+      if (nutEndReached) {
+        $('#mainWindow').append("&nbsp;");
+      }
+      //main meat of the nuts
+      else {
+        $('#mainWindow').append(dataString[stringIndex]);
+        stringIndex++;
+      }
+      cursorIndex++;
+      
+      // end of first nut
+      if (cursorIndex == nutWidth) {
+        nutEndReached = true;
+        //console.log('end reached');
+      }
+      // beginning of second nut
+      else if (cursorIndex == nutWidth + nutWidth / 3) {
+        nutEndReached = false;
+        //console.log('beginning reached');
+      }
+      // end of line
+      else if (cursorIndex == nutWidth * 2 + nutWidth / 3) {
+        lineEndReached = true;
+        console.log('line end reached');
+        $('#mainWindow').append("<br/ >");
+        linesPrinted++;
+        cursorIndex = 0;
+      }
+      // end of nuts 
+      if (linesPrinted == Math.ceil(nutWidth / 2)) {
+        console.log('end of nuts');
+        state = "SHAFT";
+        for (var i = 0; i < ((nutWidth * 2) / 3) - 1; ++i) {
+          $('#mainWindow').append("&nbsp;");
+          cursorIndex = ((nutWidth * 2) / 3) - 1;
+        }
+      }
+    }
+  }
+  else if (state == "SHAFT") {
+    for (var i = 0; i < nutWidth / 3; ++i) {
+      $('#mainWindow').append(dataString[stringIndex]);
+      stringIndex++;
+      cursorIndex++;
+      if (cursorIndex == (((nutWidth * 2) / 3) + nutWidth + 1)) {
+        console.log('end of shaft line');
+        $('#mainWindow').append("<br/ >");
+        linesPrinted++;
+        for (var i = 0; i < ((nutWidth * 2) / 3) - 1; ++i) {
+          $('#mainWindow').append("&nbsp;");
+          cursorIndex = ((nutWidth * 2) / 3) - 1;
+        }
+      }
+    }
+    
+    
+    
+  }
+  
+  
 
-var testString = "nfijsadnfkjsadhfjksdahfkndfhsaudhfsfhsdljffdsnafkndsa kfnjskeafnlkjeawjfiwekur38443289rhn34run430n9cr9pu43mhrcion34urf0983nq4ocrj3094rcoi3qc4pn4jr09jo4q3lroial4ruja3948roua3o948ornihc34on8cnrio4lariaco8w4c9or3c29uncj8i";
+  console.log('index: ', cursorIndex);
+});
