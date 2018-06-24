3f8bb341112cec1df31bce6a08bd39bffc672d54
aawill <aawill@umich.edu>
2018-06-23 22:01:52 -0400

    initialize!

---
+++ hackerpenis.js
@@ -0,0 +1,11 @@
+var stringIndex = 0;
+var nutWidth = 5;
+var shaftHeight = 12;
+
+$(document).keypress(function() {
+  stringIndex++;
+  //console.log(stringIndex);
+  $('#mainWindow').append(testString[stringIndex]);
+});
+
+var testString = "nfijsadnfkjsadhfjksdahfkndfhsaudhfsfhsdljffdsnafkndsa kfnjskeafnlkjeawjfiwekur38443289rhn34run430n9cr9pu43mhrcion34urf0983nq4ocrj3094rcoi3qc4pn4jr09jo4q3lroial4ruja3948roua3o948ornihc34on8cnrio4lariaco8w4c9or3c29uncj8i";
