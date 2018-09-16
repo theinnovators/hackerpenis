aa4f52318d5f7471d2164bf143c62877ceb67152
aawill <aawill@umich.edu>
2018-09-15 20:58:55 -0400

    not sure what but it says I changed something

--- hackerpenis.js
+++ hackerpenis.js
@@ -92,7 +92,7 @@ function changeHeight(newHeight) {
 }
 
 $(document).on('keypress touchstart', (function(event) {
-  if (event.target.type == 'radio') return;
+  if (event.target.tagName == 'radio') return;
   for (var outerIndex = 0; outerIndex < 3; ++outerIndex) {
     if (state == 'BALLS') {
       for (var i = 0; i < nutWidth / 3; ++i) {
