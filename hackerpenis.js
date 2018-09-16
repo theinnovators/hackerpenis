01e17e338dcfce3a238728db01baa68902c124e4
aawill <aawill@umich.edu>
2018-09-15 20:48:54 -0400

    focusing bug fixed

--- hackerpenis.js
+++ hackerpenis.js
@@ -88,7 +88,8 @@ function changeHeight(newHeight) {
   pendingHeightChange = '';
 }
 
-$(document).on('keypress touchstart', (function() {
+$(document).on('keypress touchstart', (function(event) {
+  if (event.target.type == 'radio') return;
   for (var outerIndex = 0; outerIndex < 3; ++outerIndex) {
     if (state == 'BALLS') {
       for (var i = 0; i < nutWidth / 3; ++i) {
